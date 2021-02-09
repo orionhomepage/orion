15c4b214fa7571674b49c35678e1928a50b75e7a
welzel855 <welzel855@hotmail.com>
2021-02-09 13:40:47 -0300

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -11,33 +11,33 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     .fromTo('.logo', 0.7,
     {y: -50, opacity: 0},
-    {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+    {y: 0, opacity: 1, ease: Expo.easeInOut})
 
     .fromTo('.nav-list', 0.7,
     {y: -50, opacity: 0},
-    {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+    {y: 0, opacity: 1, ease: Expo.easeInOut})
 
     .fromTo('.nav-social', 0.7,
     {y: -50, opacity: 0},
-    {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+    {y: 0, opacity: 1, ease: Expo.easeInOut})
 
     .fromTo('.title', 0.7,
     {y: -50, opacity: 0},
-    {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+    {y: 0, opacity: 1, ease: Expo.easeInOut})
 
     .fromTo('.desc', 0.7,
     {y: -50, opacity: 0},
-    {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+    {y: 0, opacity: 1, ease: Expo.easeInOut})
 
     .fromTo('.btn', 0.7,
     {y: -50, opacity: 0},
-    {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+    {y: 0, opacity: 1, ease: Expo.easeInOut})
 
     .fromTo('.btn-secondary', 0.7,
     {y: -50, opacity: 0},
-    {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+    {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=1')
 
     .fromTo('.jersey', 3,
     {y: -50, opacity: 0},
-    {y: 0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+    {y: 0, opacity: 1, ease: Expo.easeInOut})
 })
